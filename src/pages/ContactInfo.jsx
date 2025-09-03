6c9483fc86a6e45fff3a6e1486d89d1d6189bf73
Daniel Boyce <danielboyce@Daniels-MacBook-Air.local>
2025-09-03 10:35:17 -0400

    feat:adds links to contact info

--- src/pages/ContactInfo.jsx
+++ src/pages/ContactInfo.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const ContactInfo = () => {
   return (
     <div>
-      
+      <h1>Contact</h1>
+      <p>Feel Free to reach out anytime</p>
+      <p>daniel.boyce35@gmail.com</p>
+      <p>g</p>
     </div>
   )
 }
