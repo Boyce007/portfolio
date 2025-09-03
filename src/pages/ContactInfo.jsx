7024850bebf47054751d334ea4d902014b565f7b
Daniel Boyce <danielboyce@Daniels-MacBook-Air.local>
2025-09-03 10:48:16 -0400

    chore:adds proper links to contact page

--- src/pages/ContactInfo.jsx
+++ src/pages/ContactInfo.jsx
@@ -5,8 +5,12 @@ const ContactInfo = () => {
     <div>
       <h1>Contact</h1>
       <p>Feel Free to reach out anytime</p>
-      <p>daniel.boyce35@gmail.com</p>
-      <p>g</p>
+
+      <div class='contact-info'>
+        <a>daniel.boyce35@gmail.com</a>
+        <a href="https://github.com/Boyce007">Github</a>
+        <a href="https://www.linkedin.com/in/daniel-i-boyce-b6395a241">LinkedIn</a>
+      </div>
     </div>
   )
 }
